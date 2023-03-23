3a16bee2b508e144c607fe1293a9568d06735ac9
N1ghtm4r3 <124650099+N1ghtm4r3-DLL@users.noreply.github.com>
2023-03-23 03:17:28 +0000

    APP

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../../components/Header' 
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <Text>Pagina Home</Text>
+      <StatusBar style="auto"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
